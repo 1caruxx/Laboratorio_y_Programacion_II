f3cbe3bd35f313f1ea94b1be225ac82ac19932b5
fernandohugolareu@hotmail.com <1caruxx>
2017-05-03 11:56:06 -0300

    Clases abstractas y polimorfismo.
    Se ha aniadido el ejercicio hecho en clase relacionado con las clases abstractas.

--- 5. Herencia/ejemploDeHerenciav2/ejemploDeHerencia/conClasesAbstractas/AlumnoEgresado.cs
+++ 5. Herencia/ejemploDeHerenciav2/ejemploDeHerencia/conClasesAbstractas/AlumnoEgresado.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace conClasesAbstractas
 {
-    public class Class1
+   public sealed class  AlumnoEgresado:Alumno
     {
     }
 }
---
+++ 5. Herencia/ejemploDeHerenciav2/ejemploDeHerencia/conClasesAbstractas/Humano.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conClasesAbstractas
+{
+    //no se puede instanciar una clase abstracta.
+    public abstract class Humano
+    {
+        protected string _nombre;
+
+        public Humano(string nombre)
+        {
+            this._nombre = nombre;
+        }
+
+        public abstract void mostrar();
+
+
+    }
+}
